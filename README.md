9ad90e7b6464afc3d52af0bd4550ee4bfc36d79b
djhell <daniele@brugnara.me>
2014-06-14 10:43:59 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 redis-timed-disconnection
 =========================
+
+Get a redis connection and auto-terminate after 30 seconds.
+
+If you require a new connection, you will get the one active if present.
+
+If there is no active connection, a new one will be created. Very useful when digesting a huge queue
---
+++ index.js
@@ -0,0 +1,28 @@
+var redis = require('redis');
+
+var AUTO_CLOSE_CONNECTION = 30 * 1000; //seconds
+
+function RedisHelper() {
+  this.autoCloseTimeout = null;
+  this.client = null;
+};
+
+RedisHelper.prototype.getClient = function(port, host) {
+  this.startTimeout();
+  if (!this.client) {
+    this.client = redis.createClient(port, host);
+  }
+  return this.client;
+};
+
+RedisHelper.prototype.startTimeout = function() {
+  console.info(LOG_CTX, 'Starting redis auto-close connection timeout');
+  clearTimeout(this.autoCloseTimeout);
+  this.autoCloseTimeout = setTimeout(function() {
+    console.info(LOG_CTX, 'Redis connection closed due timeout (%d ms)', AUTO_CLOSE_CONNECTION);
+    this.client.end();
+    this.client = null;
+  }.bind(this), AUTO_CLOSE_CONNECTION);
+};
+
+module.exports = new RedisHelper();
